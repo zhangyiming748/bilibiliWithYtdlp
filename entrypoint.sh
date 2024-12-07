27f2f660b2b20cafe2f88877ac2b8f04c7cc8a98
zen <wsl on hasee>
2024-12-07 11:40:07 +0800

    after dos2unix

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/ash
+
+# 读取网址列表文件
+url_list="/list.txt"
+
+# 按行读取网址列表
+while IFS= read -r url
+do
+  # 在这里处理每个网址，例如访问网址、下载内容等
+  echo "处理网址： $url"
+  yt-dlp --cookies /cookies.txt --paths /data $url
+done < "$url_list"
