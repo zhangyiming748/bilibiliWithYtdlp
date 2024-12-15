87a4a5852223a380e3920a3d07e9f6d9b1e5e9fc
zen <mbp>
2024-12-15 22:02:57 +0800

    更改格式为 url 视频名

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/ash
 
 # 读取网址列表文件
-url_list="/list.txt"
+url_list="list.txt"
 
 # 按行读取网址列表
 while IFS= read -r url
 do
   # 在这里处理每个网址，例如访问网址、下载内容等
   echo "处理网址： $url"
-  yt-dlp --cookies /cookies.txt -f 'bestvideo[height<=?1080]+bestaudio/best[height<=?1080]/mp4' --paths /data $url
+  # 使用 awk 按空格分割并处理
+  link=$(echo "$url" | awk '{print $1}')
+  name=$(echo "$url" | awk '{print $2}')
+  echo $link
+  echo $name
+  fname=${name}.mp4
+  echo $fname
+  yt-dlp --cookies /cookies.txt -f 'bestvideo[height<=?1080]+bestaudio/best[height<=?1080]/mp4' --paths /data --output ${fname} $link
 done < "$url_list"
+# 输入的字符串按照空格分割 空格前为真正的网址 空格后为文件名 shell实现
